4bd6373aa0401ddc52d87087f20482e7c2d2ca3b
Rupali Shahi <rupali.ceinc@gmail.com>
2018-07-18 12:05:56 +0530

    @HostListner

--- src/app/blue-color.directive.ts
+++ src/app/blue-color.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[blueColored]'
@@ -7,6 +7,15 @@ export class BlueColorDirective {
 
   constructor(element: ElementRef) {
     element.nativeElement.style.color = 'blue';
+   }
+
+  //  @HostListener('click') doSomething() {
+  //    console.log('DOM element is clicked');
+  //  }
+
+  @HostListener ('document:click', ['$event'])
+   elemClicked(elem) {
+     console.log('clicked', elem);
    }
 
 }
